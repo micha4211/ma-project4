bed36c23e390c4b7ee410f229343ab7c02cba445
micha4211 <micha4211@icloud.com>
2022-11-29 19:01:58 -0600

    Test pipeline

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Upload Project 4 to Git.
+          Test Pipeline
         </p>
         <a
           className="App-link"
