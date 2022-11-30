2e330eac7345b3c946c60ddff60d352ffad0bf6b
micha4211 <micha4211@icloud.com>
2022-11-29 18:46:46 -0600

    Testing updating the app in GIT

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Upload Project 4 to Git.
         </p>
         <a
           className="App-link"
